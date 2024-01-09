d4e17016d2f7b6f3868dc7d651971aeaf9c10ace
RomanV79 <121058298+RomanV79@users.noreply.github.com>
2024-01-09 15:31:17 +0300

    feat: created Box class and tests for them

---
+++ src/test/java/assertj/BoxTest.java
@@ -0,0 +1,64 @@
+package assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class BoxTest {
+
+    @Test
+    void isThisSphere(){
+        Box box = new Box(0,10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Sphere");
+    }
+
+    @Test
+    void isNotTriangle() {
+        Box box = new Box(2, 2);
+        String name  = box.whatsThis();
+        assertThat(name).isNotEqualTo("Triangle");
+    }
+
+    @Test
+    void hasFourVertices() {
+        Box box = new Box(4,3);
+        int vertexQnt = box.getNumberOfVertices();
+        assertThat(vertexQnt).isEqualTo(4);
+    }
+
+    @Test
+    void hasNotThreeVertices() {
+        Box box = new Box(5,3);
+        int vertexQnt = box.getNumberOfVertices();
+        assertThat(vertexQnt).isNotEqualTo(3);
+    }
+
+    @Test
+    void isObjectExist() {
+        Box box = new Box(0, 1);
+        boolean exist = box.isExist();
+        assertThat(exist).isTrue();
+    }
+
+    @Test
+    void isObjectNotExist() {
+        Box box = new Box(2,2);
+        boolean exist = box.isExist();
+        assertThat(exist).isFalse();
+    }
+
+    @Test
+    void areaIsMoreThanZero() {
+        Box box = new Box(8, 8);
+        double square = box.getArea();
+        assertThat(square > 0).isTrue();
+    }
+
+    @Test
+    void areaIsZero() {
+        Box box = new Box(3, 8);
+        double square = box.getArea();
+        assertThat(square).isEqualTo(0);
+    }
+}
