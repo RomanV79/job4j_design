51c3a22468e832dca31c1187843ca5ff93ff5db8
RomanV79 <121058298+RomanV79@users.noreply.github.com>
2024-01-13 16:12:05 +0300

    feat: added exception tests
    feat: added exception tests

---
+++ src/test/java/assertj/NameLoadTest.java
@@ -0,0 +1,40 @@
+package assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkWrongFormat() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("key1value1", "key2=value2"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("the symbol");
+
+    }
+
+    @Test
+    void checkHaveKey() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("key1=value1", "=value2"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("a key");
+    }
+
+    @Test
+    void checkHaveValue() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("key1=value1", "key2="))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("a value");
+    }
+}
