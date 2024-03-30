b814e1474438019013a78c98f3fe6bc7340a0ca3
RomanV79 <121058298+RomanV79@users.noreply.github.com>
2024-03-30 16:36:12 +0300

    feat: dynamics List by array

---
+++ src/main/java/collection/SimpleArrayList.java
@@ -0,0 +1,88 @@
+package collection;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+
+    private T[] container;
+    private int size;
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        if (container.length == size) {
+            increaseContainer();
+        }
+        container[size] = value;
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        Objects.checkIndex(index, size);
+        container[index] = newValue;
+        return newValue;
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, size);
+        T value = container[index];
+        System.arraycopy(container, index + 1, container, index, container.length - index - 1);
+        size--;
+        modCount++;
+        return value;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int currentIndex = 0;
+            private int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return currentIndex < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return container[currentIndex++];
+            }
+        };
+    }
+
+    private void increaseContainer() {
+        if (container.length != 0) {
+            container = Arrays.copyOf(container, container.length * 2);
+        } else {
+            container = (T[]) new Object[1];
+        }
+
+    }
+}
